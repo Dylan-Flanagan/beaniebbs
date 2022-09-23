1e5e004a90644811963c92624a44b20f1a7cef29
Dylan <dylflanagan@gmail.com>
2022-09-23 12:07:21 -0700

    Part A = Display Beanies

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 /* Imports */
-
+import { getBeanies } from './fetch-utils.js';
+import { renderBeanie } from './render-utils.js';
 /* Get DOM Elements */
-
+const beanieList = document.getElementById('beanie-list');
 /* State */
-
+let beanies = [];
+let error = null;
 /* Events */
+window.addEventListener('load', async () => {
+    findBeanies();
+});
 
+async function findBeanies(name) {
+    const response = await getBeanies();
+    error = response.error;
+    beanies = response.data;
+    if (!error) {
+        displayBeanies();
+    }
+}
 /* Display Functions */
 function displayBeanies() {
     beanieList.innerHTML = '';
