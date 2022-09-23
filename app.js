228fe5bc78b70a64df33141850e6b06dfb67bb7f
Dylan <dylflanagan@gmail.com>
2022-09-22 18:03:08 -0700

    Added fetch-utils file, HTML and beginning of display functions

--- app.js
+++ app.js
@@ -7,5 +7,13 @@
 /* Events */
 
 /* Display Functions */
+function displayBeanies() {
+    beanieList.innerHTML = '';
+
+    for (const beanie of beanies) {
+        const beanieEl = renderBeanie(beanie);
+        beanieList.append(beanieEl);
+    }
+}
 
 // (don't forget to call any display functions you want to run on page load!)
